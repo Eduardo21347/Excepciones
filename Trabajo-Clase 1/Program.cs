c0e2357f3448377b9ba03b81741f944454216d90
Eduardo21347 <105814976+Eduardo21347@users.noreply.github.com>
2022-05-27 12:14:57 -0700

    Expeciones

---
+++ Trabajo-Clase 1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabajo_Clase_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Escribre un numero: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Escribe otro numero: ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(num1 / num2);
+                Console.ReadLine();
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
+            catch (FormatException a)
+            {
+                Console.WriteLine(a.Message);
+                Console.WriteLine(a.StackTrace);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
